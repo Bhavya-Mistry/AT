bce3881699d94e1a0e17299bf565305e8d98f4d7
KartikHajela <test2codee@gmail.com>
2026-01-05 17:12:33 +0530

    update

--- patient project/backend/db.py
+++ patient project/backend/db.py
@@ -1,12 +1,14 @@
 import urllib.parse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
- 
+import os
+from dotenv import load_dotenv
+load_dotenv()
  
 # 2. Construct the URL using the encoded password
 # Default postgres user is often 'postgres' - check if yours is 'user'
-DATABASE_URL = "postgresql://postgres:Bm%406352181842@localhost:5432/patient_portal"
- 
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
