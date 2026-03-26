2d97603a6a1ddb76a6e77e50a02a2e7e9c060065
bhavya-mistry23 <bhavya.mistry@alliancetek.com>
2026-03-26 13:28:42 +0530

    update

--- patient project/backend/main.py
+++ patient project/backend/main.py
@@ -1,36 +1,19 @@
 # --- FIX FOR PYTHON 3.12+ / BCRYPT ISSUES ---
-from sqlalchemy.orm.attributes import flag_modified
-import ai_service
 import bcrypt
 
 # Patching bcrypt
 bcrypt.__about__ = type("about", (object,), {"__version__": bcrypt.__version__})
 # --------------------------------------------
 from fastapi.staticfiles import StaticFiles
-import drive_service
-import pdf_generation_service
-import uuid
-import json
-from datetime import datetime
-from fastapi import FastAPI, Depends, HTTPException, status, UploadFile, File, Form
-import shutil
+from fastapi import FastAPI
 import os
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session, joinedload
 
 # from passlib.context import CryptContext
-from typing import List
-from fastapi.responses import StreamingResponse
 
-from db import SessionLocal, engine, get_db
+from db import engine
 import models
-import schemas
 
-
-from fastapi.security import OAuth2PasswordRequestForm
-from security import create_access_token
-
-from security import get_current_user
 
 from routers import auth, chat, user, doctor, media, appointment
 
